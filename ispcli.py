029cea6596e09fd5b9acb1b2b22e1db53526244b
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-10-18 18:04:50 +0200

    add functions

--- ispcli.py
+++ ispcli.py
@@ -43,7 +43,8 @@ def load_user_email(head, query):
     data = ispmanagerclass.list_data(head)
     return print_data(data.user_email(query), head)
 
-def load_email_setting(names, query, owner):
+def load_email_setting(names, owner):
+    query = ispmanagerclass.http_query_isp("email")
     data = ispmanagerclass.list_data(names)
     res = data.list(query)
     for r in res:
@@ -118,9 +119,8 @@ def main():
         return load_user_email(names, query)
     elif args.user:
         if args.email:
-            query = ispmanagerclass.http_query_isp("email")
             names = ["owner", "name", "forward"]
-            return load_email_setting(names,query,args.email)
+            return load_email_setting(names,args.email)
     else:
         parser.print_help()
 
