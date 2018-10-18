31b09206c1261c17d1bfa10513e1ee06809c8e40
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-10-18 19:16:21 +0200

    add functions

--- ispcli.py
+++ ispcli.py
@@ -56,7 +56,7 @@ def load_email_setting(names, user):
             setting_info_by_user = data.user_email(query)
             for setting in setting_info_by_user:
                 for key in head:
-                    if setting[key] is null:
+                    if setting[key] is None:
                         setting[key] = "Null"
                     print str(key) +" "+ str(setting[key])
 
