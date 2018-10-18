c5481424e003c46537b467f0eb38df5e27b7f67f
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-10-18 18:42:49 +0200

    add functions

--- ispcli.py
+++ ispcli.py
@@ -54,7 +54,8 @@ def load_email_setting(names, user):
             query = ispmanagerclass.URL + "&elid=" + api_data["name"] + "&func=email.edit&out=xml"
             #print (str(api_data["owner"])+" "+str(api_data["name"]))
             setting_info_by_user = data.user_email(query)
-            print setting_info_by_user["passwd"]
+            for setting in setting_info_by_user:
+                print setting["passwd"]
 
 
             #query = ispmanagerclass.URL + "&elid=" + args.email_info + "&func=email.edit&out=xml"
