9122c955c9133873ad433af8780d4f33228a4105
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-10-20 21:24:07 +0200

    add functions

--- ispcli.py
+++ ispcli.py
@@ -2,10 +2,10 @@
 # Copyright (c) 2018 Ruslan Variushkin,  ruslan@host4.biz
 # Version 0.0.2
 
-import sys
-sys.path.append("/usr/lib/python2.7/ispcli")
+#import sys
+#sys.path.append("/usr/lib/python2.7/ispcli")
 
-import app
+import ispcli.app
 
 if __name__ == "__main__":
     main().menu
