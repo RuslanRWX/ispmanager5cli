eaa321a0c4ed8e03c2cce9e9e193d7d6a444e0c8
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-10-21 14:56:54 +0200

    add functions

--- ispcli.py
+++ ispcli.py
@@ -3,9 +3,9 @@
 # Version 0.0.2
 
 import sys
-sys.path.append("/usr/local/lib/python2.7/dist-packages/")
+sys.path.append("/usr/local/lib/python2.7/dist-packages/ispcli")
 
-from ispcli.app import main
+from app import main
 
 if __name__ == "__main__":
     main()
