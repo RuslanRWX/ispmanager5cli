8356b19064a52a438fcbac9b3e84b40f63804f21
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-10-21 15:41:00 +0200

    add functions

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ chmod 500 /etc/ispcli
 cp ispcli.py /usr/sbin/ispcli 
 chmod 500 /usr/sbin/ispcli
 mkdir  $lib_path
-echo y | cp lib/help_text.py $lib_path
-echo y | cp lib/ispmanagerclass.py $lib_path
-echo y | cp lib/app.py $lib_path
-echo -y | cp  lib/main.py $lib_path
-echo -y | cp lib/__init__.py $lib_path
+echo y | cp ispcli/help_text.py $lib_path
+echo y | cp ispcli/ispmanagerclass.py $lib_path
+echo y | cp ispcli/app.py $lib_path
+echo -y | cp  ispcli/main.py $lib_path
+echo -y | cp ispcli/__init__.py $lib_path
 
 echo "Success!"
 exit 0 
