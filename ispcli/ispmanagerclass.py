5db1aa500e924559a996e5fbff8f5d3b94d9f6c3
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-11-16 15:47:46 +0100

    add functions

--- ispcli/ispmanagerclass.py
+++ ispcli/ispmanagerclass.py
@@ -103,10 +103,10 @@ class custom_task():
         doc=request_http(query)
         for node in doc.getElementsByTagName('elem'):
             for usernameBill in node.getElementsByTagName('username'):
-                return usernameBill
-       #                 if usernameBill.firstChild.nodeValue == user:
-       #                     for account in node.getElementsByTagName('account'):
-        #                        return account.firstChild.nodeValue
+                #return usernameBill
+                        if usernameBill.firstChild.nodeValue == user:
+                            for account in node.getElementsByTagName('account'):
+                                return account.firstChild.nodeValue
  #       return user
 
 
