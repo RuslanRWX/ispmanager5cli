9b3d088280db6e93c6bb42411fd5f854c376010d
Ruslan Veriushkin <ruslansvs2@yandex.ua>
2018-11-16 15:42:17 +0100

    add functions

--- ispcli/ispmanagerclass.py
+++ ispcli/ispmanagerclass.py
@@ -101,10 +101,10 @@ class custom_task():
     def bill_account(self, user):
         query = url_bill + "&func=user&out=xml"
         doc=request_http(query)
-        print query
-        for node in doc.getElementsByTagName('elem'):
-            for usernameBill in node.getElementsByTagName('username'):
-                print usernameBill
+        return query
+        #for node in doc.getElementsByTagName('elem'):
+         #   for usernameBill in node.getElementsByTagName('username'):
+          #      print usernameBill
        #                 if usernameBill.firstChild.nodeValue == user:
        #                     for account in node.getElementsByTagName('account'):
         #                        return account.firstChild.nodeValue
